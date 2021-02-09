4f252defee7f13912eafc5d2751e6c63f694e590
cmaddix321 <56734795+cmaddix321@users.noreply.github.com>
2021-02-08 22:02:33 -0500

    Create assignment1_part1.py

---
+++ assignment1_part1.py
@@ -0,0 +1,30 @@
+def listDivide(numbers, divide=2):
+    count = 0 
+    for i in numbers:
+        if (i % divide) == 0:
+            count +=1
+    
+    return count
+
+class ListDivideException (Exception):
+    pass
+    
+    """
+    The function returns the number of elements in the numbers list that are divisibleby divide
+    """
+    pass
+
+def testListDivide():
+    """
+    Test listDivide
+    """
+    assert listDivide([1,2,3,4,5]) == 2
+    assert listDivide([2,4,6,8,10]) == 5
+    assert listDivide([30, 54, 63,98, 100], divide=10) == 2
+    assert listDivide([]) == 0
+    assert listDivide([1,2,3,4,5], 1) == 5
+    
+if __name__ == "__main__":
+    testListDivide()
+    
+testListDivide()
